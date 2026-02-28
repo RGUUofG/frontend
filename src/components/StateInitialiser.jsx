8f8806769741f1f5977f800aada6f87ab9cc0fd9
Ranjith <rkranjith03@gmail.com>
2026-02-28 20:38:39 +0000

    initial commit

---
+++ src/components/StateInitialiser.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { useEffect } from "react";
+
+const StateInitialiser = ({ children }) => {
+  useEffect(() => {
+    localStorage.clear();
+    if (localStorage.getItem("completedPuzzle1") === null) {
+      localStorage.setItem("completedPuzzle1", "false");
+    }
+
+    if (localStorage.getItem("isFuse") === null) {
+      localStorage.setItem("isFuse", "false");
+    }
+
+    if (localStorage.getItem("completedPuzzle2") === null) {
+      localStorage.setItem("completedPuzzle2", "false");
+    }
+
+    if (localStorage.getItem("lightsOn") === null) {
+      localStorage.setItem("lightsOn", "false");
+    }
+  }, []);
+  return <div>{children}</div>;
+};
+
+export default StateInitialiser;
